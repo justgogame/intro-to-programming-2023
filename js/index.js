3ace863b1761eab14b63e3d8bab5eb778415923c
Aleksandr <justgogame@gmail.com>
2023-12-01 16:06:40 -0500

    Lesson 4.3: HTML Forms and DOM Practice

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const footer = document.querySelector('footer');
+
+const copyright = document.createElement('p');
+copyright.innerHTML = `Aleksandr ${thisYear}`;
+document.querySelector('footer').appendChild(copyright);
+
+const skills = ['Digital Marketing', 'IT Project Management', 'HTML', 'CSS', 'Java Script', 'Front-end', 'Computer Skills'];
+const skillsSection = document.querySelector('#skills');
+const skillsList = skillsSection.querySelector('ul');
+
+for (i = 0; i < skills.length; i++) {
+    const skill = document.createElement('li');
+    skill.innerText = `${skills[i]}`;
+    skillsList.appendChild(skill);
+}
+
+
+const messageForm = document.getElementsByName('leave_message')[0];
+const messageSection = document.querySelector('#messages');
+messageSection.style.display = 'none';
+
+
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const usersName = e.target.usersName.value;
+    const usersEmail = e.target.usersEmail.value;
+    const usersMessage = e.target.usersMessage.value;
+
+
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML += `<a href='mailto:${usersEmail}'>${usersName}</a> <span> wrote: ${usersMessage}</span>`;
+
+    messageSection.style.display = 'block'
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', (e) => {
+        const entry = e.target.parentNode;
+        entry.remove();
+        if (document.getElementById('messages_ul').getElementsByTagName('li').length < 1) {
+            messageSection.style.display = 'none';
+        }
+    })
+
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+})
